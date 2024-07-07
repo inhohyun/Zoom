47a9439f364a21b7de0bceb448ac608cc9b7a6b3
root <root@DESKTOP-ITGGK6I>
2024-07-07 17:26:54 +0900

    fix: 웹 소켓 연결 오타(connect -> connection)

--- src/server.js
+++ src/server.js
@@ -11,22 +11,14 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get(".*", (_, res) => res.redirect("/"));
 const handleLiten = () => console.log(`Listening on http://localhost:3000`);
-
-
 // app.listen(3000, handleLiten);
-
-
-
 const server = http.createServer(app); //http 서버
 const wss = new WebSocket.Server({ server }); // ws 서버
-
-
 // function handleConnection(socket) {
 //     console.log(socket)
 // }
-wss.on("connect", (socket) => {
+wss.on("connection", (socket) => {
     console.log("Connected to Browser 😀")
     socket.send("hello!!");
 })
-
 server.listen(3000, handleLiten); //http와 ws가 같은 포트를 사용
