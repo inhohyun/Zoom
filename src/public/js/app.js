5bd5db150b1e34f668b2c92dd481190799343851
root <root@DESKTOP-ITGGK6I>
2024-07-07 17:23:07 +0900

    feat: 웹소켓으로 서버와 연결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
-hello
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server 😀");
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this: ", message, "form the Server");
+})
+
+socket.addEventListener("close", () => {
+    console.log("Connected to Server 😥");
+})
--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+import http from "http";
+import WebSocket from "ws";
+
 import express from "express";
 
 const app = express();
@@ -5,9 +8,25 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get(".*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get(".*", (_, res) => res.redirect("/"));
 const handleLiten = () => console.log(`Listening on http://localhost:3000`);
 
 
-app.listen(3000);
+// app.listen(3000, handleLiten);
+
+
+
+const server = http.createServer(app); //http 서버
+const wss = new WebSocket.Server({ server }); // ws 서버
+
+
+// function handleConnection(socket) {
+//     console.log(socket)
+// }
+wss.on("connect", (socket) => {
+    console.log("Connected to Browser 😀")
+    socket.send("hello!!");
+})
+
+server.listen(3000, handleLiten); //http와 ws가 같은 포트를 사용
